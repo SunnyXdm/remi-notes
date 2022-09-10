8bb1ff11b143b439adbaf4535b26e3d05c4cf423
Sunnyxdm <sunnyxdm@users.noreply.github.com>
2022-09-11 04:20:48 +0530

    first commit

---
+++ src/components/NoteCard.jsx
@@ -0,0 +1,16 @@
+import { Card, Text } from "@nextui-org/react";
+
+export default function NoteCard() {
+    return (
+        <Card
+            isPressable
+            isHoverable
+            variant="bordered"
+            css={{ mw: "400px" }}
+        >
+            <Card.Body>
+                <Text>A pressable card.</Text>
+            </Card.Body>
+        </Card>
+    );
+}
